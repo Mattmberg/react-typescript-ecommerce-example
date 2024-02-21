8bb6b337390eb6c1b714107d4e276c256deca05d
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2024-02-20 21:08:52 -0500

    Adding different product pages.

---
+++ react-typescript-ecommerce-example/src/clothes.tsx
@@ -0,0 +1,55 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import { Header } from './components/Header';
+import { NavBar } from './components/NavBar';
+import { Footer } from './components/Footer';
+
+function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products/category/mens-shirts')
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+      throw response;
+    })
+    .then(data => {
+      setData(data);
+    })
+    .catch(error => {
+      console.error("Error fecthing data: ", error);
+      setError(error);
+    })
+    .finally(() => {
+      setLoading(false);
+    })
+  }, [])
+
+  if (loading) return "Loading...";
+  if (error) return "Error!";
+
+  return (
+    <div className="App">
+      <Header/>
+      <NavBar/>
+      <div className='container'>
+        {data.products.map(product => {
+          return (
+            <div key={product.id}>
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <p>${product.price}</p>
+            </div>
+          )
+        })}
+      </div>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
